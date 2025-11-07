1337e94ed67ecf36e5d3e8927a2dbd873926e5da
utkarshbito <utkarsh.sharma@bito.ai>
2025-11-07 18:44:09 +0530

    Create Help.java

---
+++ Help.java
@@ -0,0 +1,33 @@
+public class Help {
+    public static void main(String[] args) {
+        int[] nums = new int[3];
+        nums[3] = 10;
+        String s = null;
+        System.out.println(s.length());
+        int result = divide(10, 0);
+        System.out.println("Result: " + result);
+        java.util.List<String> list = new ArrayList<>();
+        list.add("a");
+        System.out.println(getNumber());
+        helperInstance();
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static int getNumber() {
+        return "not a number";
+    }
+
+    public static void helperInstance() {
+        Helper h = new Helper();
+        h.doHelp();
+    }
+}
+
+class Helper {
+    public void doHelp() {
+        doHelp();
+    }
+}
